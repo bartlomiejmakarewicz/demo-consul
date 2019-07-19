3baf576c1004be12e6bac0329b9de33362964a25
Bartlomiej Makarewicz <bartlomiej.makarewicz@gmail.com>
2019-07-19 11:12:46 +0200

    Polish retry config for compose

--- src/main/kotlin/com/example/democonsul/DemoConsulApplication.kt
+++ src/main/kotlin/com/example/democonsul/DemoConsulApplication.kt
@@ -2,12 +2,8 @@ package com.example.democonsul
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient
-import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
-@EnableFeignClients
-@EnableDiscoveryClient
 class DemoConsulApplication
 
 fun main(args: Array<String>) {
